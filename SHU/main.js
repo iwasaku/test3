3165946035e3a3b9ced1eb45eaabc54cb4a237c0
岩崎 剛 <iwasakitakeshi@MacBook-Air.local>
2019-01-06 20:56:56 +0900

    失敗BGMの定義を修正

--- SHU/main.js
+++ SHU/main.js
@@ -24,8 +24,8 @@ var ASSETS = {
     "bg_yuka_1": "./resource/bg_yuka_1.png",
     "bg_yuka_2": "./resource/bg_yuka_2.png",
 
-    "fallSE": "https://iwasaku.github.io/test3/SHU/resource/fall.mp3",    // 開発時用（mp3はfile://でのアクセスが拒否されるので、https://経由にする）
-    //"fallSE": "./resource/fall.mp3",
+    //"fallSE": "https://iwasaku.github.io/test3/SHU/resource/fall.mp3",    // 開発時用（mp3はfile://でのアクセスが拒否されるので、https://経由にする）
+    "fallSE": "./resource/fall.mp3",
 };
 
 // 定義
